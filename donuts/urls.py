a72fb2cb7e27298dacf99f34a6fd01c23572b20e
Louis Merlin <louis.merlin@epfl.ch>
2015-10-12 20:30:48 +0200

    Working Donuts with nice interfacegit add *

--- donuts/urls.py
+++ donuts/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # ex: /5/
-    url(r'^(?P<donut_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- donuts/views.py
+++ donuts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Donut
+from django.contrib.auth.models import User
+from django.db.models import Count
 
 def index(request):
     latest_donut_list = Donut.objects.order_by('-pub_date')[:5]
-    context = {'latest_donut_list': latest_donut_list}
+    best_user_list = User.objects.annotate(donut_count=Count('donut')).order_by('-donut_count')[:3]
+    context = {
+        'latest_donut_list': latest_donut_list,
+        'best_user_list': best_user_list,
+    }
     return render(request, 'donuts/index.html', context)
-
-def detail(request, donut_id):
-    return HttpResponse("You're looking at donut %s." % donut_id)
