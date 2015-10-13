5c914cc63bc083d3a6a9fa7d95e638f3d34dba2b
Louis Merlin <louis.merlin@epfl.ch>
2015-10-13 23:50:10 +0200

    Better interface

--- donuts/models.py
+++ donuts/models.py
@@ -5,3 +5,5 @@ class Donut(models.Model):
     donut_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date given')
     user = models.ForeignKey(User)
+    def __str__(self):
+        return (self.user.first_name+' - '+self.donut_text)
--- donuts/views.py
+++ donuts/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 from django.db.models import Count
 
 def index(request):
-    latest_donut_list = Donut.objects.order_by('-pub_date')[:5]
-    best_user_list = User.objects.annotate(donut_count=Count('donut')).order_by('-donut_count')[:3]
+    latest_donut_list = Donut.objects.order_by('-pub_date')[:10]
+    best_user_list = User.objects.annotate(donut_count=Count('donut')).order_by('-donut_count')[:10]
     context = {
         'latest_donut_list': latest_donut_list,
         'best_user_list': best_user_list,
